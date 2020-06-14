3aa202745e8f19f069eb4128e615d5449467d17e
mloning <markus.loning.17@ucl.ac.uk>
2020-06-14 11:56:12 +0100

    fixed link to data set

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -17,7 +17,7 @@ def _load_data():
     https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN
     /6C3JR1
     """
-    return pd.read_csv("data/chemical_process_data.csv", index_col=0,
+    return pd.read_csv("../data/chemical_process_data.csv", index_col=0,
                        header=[0, 1])
 
 
